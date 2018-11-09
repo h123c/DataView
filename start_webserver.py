07f229be4ca55b295b2d4f7a97c506ba63f80f60
hc <1412002323@qq.com>
2018-11-09 20:37:03 +0800

    add deal each file in folder

--- start_webserver.py
+++ start_webserver.py
@@ -23,6 +23,7 @@ urls = (
     '/pagechange', 'pageChange',
     '/url_notfond', 'url_NotFond',
     '/params_notfond', 'paramsError',
+    '/filechange', 'fileChange',
 )
 app = web.application(urls, globals())
 # 模板公共变量
@@ -52,7 +53,8 @@ class Index:
         form2 = input_path_form()
 
         return render.index(form1,form2)
-
+global step
+step = 10
 # request 类
 class RequestData:
     def POST(self):
@@ -88,6 +90,10 @@ class RequestData:
 class Local:
     def POST(self):
         local_path= web.input(request_url=None,params=None)
+        global local_url
+        global file_name
+        global data_type
+        global file_type
         local_url = local_path["local_url"]
         file_name = local_path["file_name"]
         data_type = local_path["data_type"]
@@ -96,13 +102,23 @@ class Local:
             local_url = local_url+"/"
         if not os.path.exists(local_url):
             raise web.seeother('/url_notfond')
-
-        if not os.path.exists(local_url+file_name):
-            raise web.seeother('/params_notfond')
+        if file_name == "": #如果不输入文件名,认为需要读取整个文件夹的数据
+            global file_list
+            file_list = os.listdir(local_url)
+            if len(file_list) < 1:   #没有文件
+                raise web.seeother('/params_notfond')
+        else:
+            if not os.path.exists(local_url+file_name):
+                raise web.seeother('/params_notfond')
+            else:
+                file_list = [file_name]
         global data
         step = 10
+        read_path = local_url + file_list[0]
+        global name
+        name = file_list[0]
         if file_type == "json":
-            with open(local_url+file_name,"r") as f:
+            with open(read_path,"r") as f:
                 data_dict = json.load(f)
             if data_type == "essay":
                 data = []
@@ -113,7 +129,7 @@ class Local:
                 page_num = int(math.ceil(len(data)*1.0 / step))
                 global page_list
                 page_list = range(0, len(data), step)
-                return render.local_essay_view(data,page_num,page_list,i)
+                return render.local_essay_view(data,page_num,page_list,i,file_list,name)
             elif data_type == "filter":
                 i = 0
                 data = data_dict
@@ -121,9 +137,9 @@ class Local:
                 page_num = int(math.ceil(len(data)*1.0 /step))
                 global page_list
                 page_list = range(0,len(data),step)
-                return render.local_filter_view(data,page_num,page_list,i)
+                return render.local_filter_view(data,page_num,page_list,i,file_list,name)
         elif file_type == "csv":
-            df = pd.read_csv(local_url+file_name)
+            df = pd.read_csv(read_path)
             if data_type == "filter":
                 list_name = [column for column in df]
                 data = []
@@ -137,7 +153,7 @@ class Local:
                 page_num = int(math.ceil(len(data)*1.0 / step))
                 global page_list
                 page_list = range(0, len(data), step)
-                return render.local_filter_view_csv(data, page_num, page_list, i)
+                return render.local_filter_view_csv(data, page_num, page_list, i,file_list,name)
 
 # 换页类
 class pageChange:
@@ -158,13 +174,58 @@ class pageChange:
         elif aim == "all":
             i = int(-1)
         if file_name == "lfv":
-            return render.local_filter_view(data, page_num, page_list,i)
+            return render.local_filter_view(data, page_num, page_list,i,file_list,name)
         elif file_name == "lfvc":
-            return render.local_filter_view_csv(data, page_num, page_list, i)
+            return render.local_filter_view_csv(data, page_num, page_list, i,file_list,name)
         elif file_name == "lev":
-            return render.local_essay_view(data, page_num, page_list, i)
+            return render.local_essay_view(data, page_num, page_list, i,file_list,name)
         elif file_name == "oev":
             return render.online_essay_view(data, page_num, page_list, i)
+
+#换文件类
+class fileChange:
+    def GET(self):
+        pageChange_info = web.input()
+        aim_file = pageChange_info["aimfile"]
+        data_type = pageChange_info["type"]
+        read_path = local_url + aim_file
+        global name
+        name = aim_file
+        i = 0
+        global data
+        if data_type == "lfv":
+            with open(read_path, "r") as f:
+                data_dict = json.load(f)
+            data = data_dict
+            page_num = int(math.ceil(len(data) * 1.0 / step))
+            page_list = range(0, len(data), step)
+            return render.local_filter_view(data, page_num, page_list, i, file_list,name)
+            #return render.test(data, page_num, page_list, i, file_list,name)
+        elif data_type == "lfvc":
+            df = pd.read_csv(read_path)
+            list_name = [column for column in df]
+            data = []
+            for i in range(0, len(df)):
+                line = {}
+                for key in list_name:
+                    line[key] = df.iloc[i][key]
+                data.append(line)
+            i = 0
+            global page_num
+            page_num = int(math.ceil(len(data) * 1.0 / step))
+            global page_list
+            page_list = range(0, len(data), step)
+            return render.local_filter_view_csv(data, page_num, page_list, i, file_list,name)
+            #return render.test(data, page_num, page_list, i, file_list,name)
+        elif data_type == "lev":
+            with open(read_path, "r") as f:
+                data_dict = json.load(f)
+            data = []
+            for k in data_dict.keys():
+                data.append(data_dict[k])
+            page_num = int(math.ceil(len(data) * 1.0 / step))
+            page_list = range(0, len(data), step)
+            return render.local_essay_view(data, page_num, page_list, i, file_list,name)
 
 # 定义404错误显示内容
 class url_NotFond:
